c1fc27323e788d31363c3153ecb5cefc691543b7
aapocius <pocius.andrew@gmail.com>
2022-01-31 21:36:25 +0200

    1

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ const swiper = new Swiper('.swiper', {
       }
   }
 });
+
+
+function InvalidMsg(textbox) {
+    
+    if (textbox.value == '') {
+        textbox.setCustomValidity('Required email address');
+    }
+    else if(textbox.validity.typeMismatch){
+        textbox.setCustomValidity('please enter a valid email address');
+    }
+    else {
+        textbox.setCustomValidity('');
+    }
+    return true;
+}
